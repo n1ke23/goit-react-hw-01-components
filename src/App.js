8ee970c25295a53c1ae71841385935d6232c5b37
Oleksandr Yashin <aveodenis@gmail.com>
2020-10-28 07:14:21 +0200

    добавил немного кода и запутался с объектом статуса

--- src/App.js
+++ src/App.js
@@ -3,9 +3,10 @@ import './App.css';
 import users from './data/user.json'
 
 function App() {
+
   return (
     <>
-      <Profile users={users}/>
+      <Profile user={users} />
     </>
   );
 }
--- src/Component/Profile/Profile.js
+++ src/Component/Profile/Profile.js
@@ -1,33 +1,39 @@
 import React from 'react';
-import Stats from '../Stats/Stats';
+import Status from '../Status/Status';
 
 
-const Profile = ({users}) => {
+const Profile = ({ user }) => {
+  console.log(user);
+  // console.log(user.stats);
   return (
 
     <div className="profile">
       <div className="description">
         <img
-          src={users.avatar}
+          src={user.avatar}
           alt="user avatar"
           class="avatar"
         />
-        <p className="name">{users.name}</p>
-        <p className="tag">{users.tag}</p>
-        <p className="location">{users.location}</p>
+        <p className="name">{user.name}</p>
+        <p className="tag">{user.tag}</p>
+        <p className="location">{user.location}</p>
       </div>
 
       <ul className="stats">
 
-        <Stats stats/>
+        {/* <Stats obj={user.stats} /> */}
 
         <li>
-  <span className="label">{users.stats.}</span>
-          <span className="quantity">1000</span>
+          <span class="label">Followers</span>
+          <span class="quantity">1000</span>
         </li>
         <li>
-          <span className="label">Views</span>
-          <span className="quantity">2000</span>
+          <span class="label">Views</span>
+          <span class="quantity">2000</span>
+        </li>
+        <li>
+          <span class="label">Likes</span>
+          <span class="quantity">3000</span>
         </li>
 
       </ul>
